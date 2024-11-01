f4f742ff4fd32cddb1e9496183a36e63370a78fe
Erik Bremstedt <erik.bremstedt@gmail.com>
2024-11-01 16:58:53 +0100

    Add helper for intervals

--- koltrast/interval.py
+++ koltrast/interval.py
@@ -3,7 +3,7 @@
 from dataclasses import dataclass
 from pendulum import DateTime
 from typing import List
-from pendulum import parse
+from pendulum import parse, now
 from koltrast.chunks import Chunk, parse_chunk, add_chunk, is_half_chunk
 
 
@@ -84,3 +84,27 @@ def generate_intervals(since: str, until: str, chunk: str) -> List[Interval]:
         interval=Interval(since=since, until=until),
         chunk=parse_chunk(string=chunk)
     )
+
+
+def get_last_complete_interval(chunk: Chunk) -> Interval:
+    """
+    Returns the last complete interval of a specified type (Chunk.HOUR, Chunk.DAY, Chunk.MONTH).
+
+    :param interval_type: Type of interval (IntervalType.HOUR, IntervalType.DAY, or IntervalType.MONTH)
+    :return: Interval object representing the period from the beginning of the last complete interval
+             until its end.
+    """
+    right_now = now()
+
+    if chunk == Chunk.HOUR:
+        latest_complete_interval_start = right_now.subtract(hours=1).start_of("hour")
+    elif chunk == Chunk.DAY:
+        latest_complete_interval_start = right_now.subtract(days=1).start_of("day")
+    elif chunk == Chunk.MONTH:
+        latest_complete_interval_start = right_now.subtract(months=1).start_of("month")
+    else:
+        raise ValueError("Invalid chunk. Use Chunk.HOUR, Chunk.DAY, or Chunk.MONTH.")
+
+    latest_complete_interval_end = latest_complete_interval_start.end_of(chunk.value)
+
+    return Interval(since=latest_complete_interval_start, until=latest_complete_interval_end)
